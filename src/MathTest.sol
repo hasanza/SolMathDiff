90e09f7ea4d1d86b269978c82f8c871271912a1a
hasanza <hr90@student.london.ac.uk>
2023-04-27 21:41:03 +0500

    additional setup

---
+++ src/MathTest.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4 <0.9.0;
+import { Math as OZFP} from "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
+import { FixedPointMathLib as SolmateFP} from "../lib/solmate/src/utils/FixedPointMathLib.sol";
+import {PRBMathCommon as PRBFP} from "../lib/prb-math/src/Common.sol";
+import {FixedPointMathLib as SoladyFP} from "../lib/solady/src/utils/FixedPointMathLib.sol";
+
+contract MathTest {
+
+    constructor () {
+        
+    }
+}
